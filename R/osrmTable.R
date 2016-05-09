cbd2a5979993a24363a3cadf93350271a7b643aa
Timothée Giraud <timothee.giraud@ums-riate.fr>
2016-05-09 17:53:04 +0000

    version 3.0.0

--- R/osrmTable.R
+++ R/osrmTable.R
@@ -1,229 +1,134 @@
 #' @name osrmTable
 #' @title Get Travel Time Matrices Between Points
-#' @description Build and send OSRM API queries to get travel time matrices between points. 
-#' This function interface the \emph{table} OSRM service. 
-#' @param loc data frame containing points identifiers, longitudes and latitudes 
-#' (WGS84). If loc parameter is used, all pair-wise distances are computed.
-#' @param locId identifier field in loc.
-#' @param locLat latitude field in loc.
-#' @param locLon longitude field in loc.
-#' @param src data frame containing origin points identifiers, longitudes and latitudes 
-#' (WGS84). If dst and src parameters are used, only pairs between scr/dst are computed.
-#' @param srcId identifier field in src
-#' @param srcLat latitude fiels in src
-#' @param srcLon longitude field in src
-#' @param dst data frame containing destination points identifiers, longitudes and latitudes 
-#' (WGS84). If dst and src parameters are used, only pairs between scr/dst are computed.
-#' @param dstId identifier field in dst
-#' @param dstLat latitude field in dst
-#' @param dstLon longitude field in dst
+#' @description Build and send OSRM API queries to get travel time matrices 
+#' between points. This function interfaces the \emph{table} OSRM service. 
+#' @param loc a data frame containing 3 fields: points identifiers, longitudes 
+#' and latitudes (WGS84). It can also be a SpatialPointsDataFrame or a 
+#' SpatialPolygonsDataFrame, then row names are used as identifiers.
+#' If loc parameter is used, all pair-wise distances are computed.
+#' @param src a data frame containing origin points identifiers, longitudes 
+#' and latitudes (WGS84). It can also be a SpatialPointsDataFrame or a 
+#' SpatialPolygonsDataFrame, then row names are used as identifiers. 
+#' If dst and src parameters are used, only pairs between scr/dst are computed.
+#' @param dst a data frame containing destination points identifiers, longitudes 
+#' and latitudes (WGS84). It can also be a SpatialPointsDataFrame or a 
+#' SpatialPolygonsDataFrame, then row names are used as identifiers. 
 #' @return A list containing 3 data frames is returned. 
-#' distance_table is the matrix of travel times (in minutes), 
-#' source_coordinates and destination_coordinates are the coordinates of 
-#' the origin and destination points actually used to compute the travel times.
+#' durations is the matrix of travel times (in minutes), 
+#' sources and destinations are the coordinates of 
+#' the origin and destination points actually used to compute the travel 
+#' times (WGS84).
+#' @details If loc, src or dst are data frames we assume that the 3 first 
+#' columns of the data.frame are: identifiers, longitudes and latitudes. 
 #' @note The public OSRM API does not allow more than 10 000 
 #' distances in query result. \cr
 #' If you use an other OSRM API service, make sure that 
 #' more distances are allowed in results 
 #' (i.e. the "max-table-size" argument, Max. locations supported in distance 
 #' table query).
+#' @seealso \link{osrmIsochrone}
 #' @examples
 #' \dontrun{
 #' # Load data
 #' data("com")
+#' 
+#' # Inputs are data frames  
 #' # Travel time matrix
-#' distCom <- osrmTable(loc = com[1:50,], 
-#'                      locId = "comm_id", 
-#'                      locLat = "lat", 
-#'                      locLon = "lon")
+#' distCom <- osrmTable(loc = com[1:50, c("comm_id","lon","lat")])
 #' # First 5 rows and columns
-#' distCom$distance_table[1:5,1:5]
-#' distCom2 <- osrmTable(src = com[1:50,], 
-#'                       srcId = "comm_id", srcLat = "lat", srcLon = "lon", 
-#'                       dst = com[51:100,], 
-#'                       dstId = "comm_id", dstLat = "lat", dstLon = "lon")
+#' distCom$durations[1:5,1:5]
+#' 
+#' # Travel time matrix with different sets of origins and destinations
+#' distCom2 <- osrmTable(src = com[1:10,c("comm_id","lon","lat")],
+#'                       dst = com[11:20,c("comm_id","lon","lat")])
 #' # First 5 rows and columns
-#' distCom2$distance_table[1:5,1:5]
+#' distCom2$durations[1:5,1:5]
+#' 
+#' 
+#' # Inputs are SpatialPointsDataFrames
+#' distCom <- osrmTable(loc = src)
+#' # First 5 rows and columns
+#' distCom$durations[1:5,1:5]
+#' 
+#' # Travel time matrix with different sets of origins and destinations
+#' distCom2 <- osrmTable(src = src, dst = dst)
+#' # First 5 rows and columns
+#' distCom2$durations[1:5,1:5]
 #' }
 #' @export
-osrmTable <- function(loc, locId, locLat, locLon, 
-                      src = NULL, srcId, srcLat, srcLon, 
-                      dst = NULL, dstId, dstLat, dstLon){
+osrmTable <- function(loc, src = NULL, dst = NULL){
   tryCatch({
     if (is.null(src)){
-      nSrc <- nrow(loc)
-      nDst <- nSrc
-      osrmLimit(nSrc = nSrc, nDst = nDst)
-      # Build the query
-      req <- tableLoc(loc = loc, locLat = locLat, locLon = locLon)
-      # Get the result
-      resRaw <- RCurl::getURL(utils::URLencode(req), 
-                              useragent = "'osrm' R package")
-      # Parse the results
-      res <- jsonlite::fromJSON(resRaw)
-      # get the distance table
-      distance_table <- distTableFormat(res = res, 
-                                        src = loc, srcId = locId, 
-                                        dst = loc, dstId = locId)
-      # get the coordinates
-      coords <- coordFormat(res = res, 
-                            src = loc, srcId = locId, 
-                            dst = loc, dstId = locId)
-      return(list(distance_table = distance_table, 
-                  source_coordinates = coords$source_coordinates, 
-                  destination_coordinates = coords$destination_coordinates))
+      # check if inpout is sp, transform and name columns
+      if(testSp(loc)){
+        loc <- spToDf(x = loc)
+      }else{
+        names(loc) <- c("id", "lon", "lat")
+      }
       
-    } else {
-      nSrc <- nrow(src)
-      nDst <- nrow(dst)
-      osrmLimit(nSrc = nSrc, nDst = nDst)
+      # Check query size
+      osrmLimit(nSrc = nrow(loc), nDst = nrow(loc))
+      
+      # Format
+      src <- loc
+      dst <- loc
+
       # Build the query
-      req <- tableSrcDst(src = src, srcLat = srcLat, srcLon = srcLon, 
-                         dst = dst, dstLat = dstLat, dstLon = dstLon)
-      # Get the result
-      resRaw <- RCurl::getURL(utils::URLencode(req), 
-                              useragent = "'osrm' R package")
-      # Parse the results
-      res <- jsonlite::fromJSON(resRaw)
-      # get the distance table
-      distance_table <- distTableFormat(res = res, 
-                                        src = src, srcId = srcId, 
-                                        dst = dst, dstId = dstId)
-      # get the coordinates
-      coords <- coordFormat(res = res, 
-                            src = src, srcId = srcId, 
-                            dst = dst, dstId = dstId)
-      return(list(distance_table = distance_table, 
-                  source_coordinates = coords$source_coordinates, 
-                  destination_coordinates = coords$destination_coordinates))
+      req <- tableLoc(loc = loc)
+    }else{
+      # check if inpout is sp, transform and name columns
+      if(testSp(src)){
+        src <- spToDf(x = src)
+      }else{
+        names(src) <- c("id", "lon", "lat")
+      }
+      # check if inpout is sp, transform and name columns
+      if(testSp(dst)){
+        dst <- spToDf(x = dst)
+      }else{
+        names(dst) <- c("id", "lon", "lat")
+      }
+      
+      # Check query size
+      osrmLimit(nSrc = nrow(src), nDst = nrow(dst))
+      
+      # Build the query
+      loc <- rbind(src, dst)
+
+      req <- paste(tableLoc(loc = loc),
+                   "?sources=", 
+                   paste(0:(nrow(src)-1), collapse = ";"), 
+                   "&destinations=", 
+                   paste(nrow(src):(nrow(loc)-1), collapse = ";"), 
+                   sep="")
     }
-  }, error=function(e) { message("osrmTable function returns an error: \n", e)})
+
+    # Get the result
+    resRaw <- RCurl::getURL(utils::URLencode(req), 
+                            useragent = "'osrm' R package")
+    # Error if URL is too long
+    e <- simpleError("The URL sent to the OSRM public API is too long. Enter less 
+         sources or destinations.")
+    if(getOption("osrm.server") == "http://router.project-osrm.org/" & resRaw==""){
+      stop(e)
+    }
+    # Parse the results
+    res <- jsonlite::fromJSON(resRaw)
+    
+    # Check results
+    e <- simpleError(res$message)
+    if(res$code != "Ok"){stop(e)}
+    
+    # get the distance table
+    durations <- distTableFormat(res = res, src = src, dst = dst)
+    
+    # get the coordinates
+    coords <- coordFormat(res = res, src = src, dst = dst)
+    
+    return(list(durations = durations, 
+                sources = coords$sources, 
+                destinations = coords$destinations))
+  }, error=function(e) {message("osrmTable function returns an error: \n", e)})
   return(NULL)
 }
-
-distTableFormat <- function(res, src, srcId, dst, dstId){
-  # extract distance table
-  mat <- res$distance_table
-  # From millisec to minutes
-  mat <- round(mat/(600), 1)
-  # NA management
-  mat[mat == 3579139.4] <- NA
-  # col and row names management
-  dimnames(mat) <- list(src[, srcId], dst[, dstId])
-  return(mat)
-}  
-
-coordFormat <- function(res, src, srcId, dst, dstId){
-  source_coordinates <- res$source_coordinates
-  row.names(source_coordinates) <- src[,srcId]
-  colnames(source_coordinates) <- c("Lat", "Lon")
-  destination_coordinates <- res$destination_coordinates
-  row.names(destination_coordinates) <- dst[,dstId]
-  colnames(destination_coordinates) <- c("Lat", "Lon")
-  return(list(source_coordinates = source_coordinates, 
-              destination_coordinates = destination_coordinates)
-  )
-}
-
-
-tableLoc <- function(loc, locLat, locLon){
-  # Query build
-  tab <- paste(getOption("osrm.server"), "table?loc=", sep = "")
-  n <- nrow(loc)
-  for (i in 1:n){
-    tab <- paste(tab, loc[i, locLat], ",", loc[i, locLon], "&loc=", sep="")
-  }
-  tab <- substr(x = tab, start = 1, stop = (nchar(tab)-5))
-  return(tab)
-}
-
-tableSrcDst <- function(src, srcLat, srcLon,
-                        dst, dstLat, dstLon){
-  # Query build
-  tab <- paste(getOption("osrm.server"), "table?src=", sep = "")
-  n <- nrow(src)
-  for (i in 1:n){
-    tab <- paste(tab, src[i,srcLat], ",", src[i,srcLon], "&src=", sep="")
-  }
-  tab <- substr(x = tab, start = 1, stop = (nchar(tab)-5))
-  tab <- paste(tab, "&dst=", sep = "")
-  m <- nrow(dst)
-  for (i in 1:m){
-    tab <- paste(tab, dst[i,dstLat], ",", dst[i,dstLon], "&dst=", sep="")
-  }
-  tab <- substr(x = tab, start = 1, stop = (nchar(tab)-5))
-  return(tab)
-}
-
-osrmLimit <- function(nSrc, nDst){
-  e <- simpleError("The public OSRM API does not allow results with 
-  a number of distances higher than 10000")
-  if(getOption("osrm.server") == "http://router.project-osrm.org/" & 
-     (nSrc*nDst) > 10000){
-    stop(e)
-  }
-}
-
-
-# src = com
-# srcId = "comm_id"
-# srcLat = "lat"
-# srcLon = "lon"
-# dst = com[1,]
-# dstId = "comm_id"
-# dstLat = "lat"
-# dstLon = "lon"
-# 
-# nSrc <- nrow(src)
-# nDst <- nrow(dst)
-# dimMat <- nSrc * nDst
-# d_t <- matrix(data = 1:dimMat, nrow = nSrc, ncol = nDst, 
-#               dimnames = list(src[,srcId], dst[, dstId]))
-# source_coordinates <- data.frame(row.names = src[,srcId], 
-#                                  Lat = rep(NA, nSrc), 
-#                                  Lon = rep(NA, nSrc))
-# destination_coordinates <- data.frame(row.names = dst[,dstId], 
-#                                       Lat = rep(NA, nDst), 
-#                                       Lon = rep(NA, nDst))
-# 
-# 
-# # nb de colonnes Full pour une requetes
-# nFullCol1Req <- 10000 %/% nSrc
-# 
-# # nb de requetes full necessaire
-# nReqFullCol <- nDst %/% nFullCol1Req
-# 
-# # nb col restantes
-# nColRest <- nDst - nReqFullCol * nFullCol1Req
-# 
-# init <- 1
-# for (i in 1:nReqFullCol){
-#   req <- tableSrcDst(src = src, srcLat = srcLat, srcLon = srcLon, 
-#                      dst = dst[init:(init+nFullCol1Req-1),], 
-#                      dstLat = dstLat, dstLon = dstLon)
-#   resRaw <- RCurl::getURL(utils::URLencode(req), 
-#                           useragent = "'osrm' R package")
-#   res <- jsonlite::fromJSON(resRaw)
-#   print(i)
-#   distance_table <- distTableFormat(res = res, 
-#                                     src = src, srcId = srcId, 
-#                                     dst = dst[init:(init+nFullCol1Req-1),], 
-#                                     dstId = dstId)
-#   d_t[,init:(init+nFullCol1Req-1)] <- distance_table
-#   init <- init+nFullCol1Req
-# }
-# 
-# # Pour les autres colonnes
-# req <- tableSrcDst(src = src, srcLat = srcLat, srcLon = srcLon, 
-#                    dst = dst[init:nDst,], 
-#                    dstLat = dstLat, dstLon = dstLon)
-# resRaw <- RCurl::getURL(utils::URLencode(req), 
-#                         useragent = "'osrm' R package")
-# res <- jsonlite::fromJSON(resRaw)
-# 
-# distance_table <- distTableFormat(res = res, 
-#                                   src = src, srcId = srcId, 
-#                                   dst = dst[init:nDst,], 
-#                                   dstId = dstId)
-# d_t[,init:nDst] <- distance_table
 
--- README.md
+++ README.md
@@ -1,38 +1,134 @@
 # osrm R package
 
-[![Version](http://www.r-pkg.org/badges/version/osrm)](https://cran.rstudio.com/web/packages/osrm/)
-![](http://cranlogs.r-pkg.org/badges/grand-total/osrm?color=brightgreen)  
+[![Version](http://www.r-pkg.org/badges/version/osrm)](https://CRAN.R-project.org/package=osrm/)
+![](http://cranlogs.r-pkg.org/badges/osrm?color=brightgreen)
+[![Travis-CI Build Status](https://travis-ci.org/rCarto/osrm.svg?branch=master)](https://travis-ci.org/rCarto/osrm)
+![](https://img.shields.io/badge/license-GPL--3-brightgreen.svg?style=flat)  
 
+***Interface Between R and the OpenStreetMap-Based Routing Service [OSRM](http://project-osrm.org/)***
 
-***Shortest Paths and Travel Time from OpenStreetMap via an OSRM API / Temps de trajet et matrices de distances entre points avec OpenStreetMap via une [API OSRM](http://project-osrm.org/)***
-
-![](http://f.hypotheses.org/wp-content/blogs.dir/1909/files/2015/10/route1.png)
+![](https://f.hypotheses.org/wp-content/blogs.dir/1909/files/2016/02/Rroads.png)
 
 ## Description
 OSRM is a routing service based on OpenStreetMap data. See <http://project-osrm.org/> for more information. A public API exists but one can run its own instance. This package allows to compute distance (travel time and kilometric distance) between points and travel time matrices.
 
-This package relies on the usage of a running OSRM service. By default this service is the OSRM public API (http://router.project-osrm.org/). To change the OSRM server, change the osrm.server option:
+This package relies on the usage of a running OSRM service (tested with version 5.0.0 of the OSRM API). 
+By default this service is the OSRM public API (http://router.project-osrm.org/). To change the OSRM server, change the osrm.server option:
 `options(osrm.server = "http://address.of.the.server/")`
 
 ## Features
 
 * `osrmTable` Get travel time matrices between points.
 
-* `osrmViaRoute` Get travel time and travel distance between two points.
+* `osrmRoute` Get the shortest path between two points.
 
-* `osrmViaRouteGeom` Get the travel geometry between two points.
+* `osrmTrip` Get the travel geometry between multiple unordered points.
+
+* `osrmIsochrone` Get a SpatialPolygonsDataFrame of isochrones.
+
+
+## Demo
+
+### `osrmTable`
+
+```r
+library(osrm)
+# Load data
+data("com")
+# Travel time matrix
+distCom <- osrmTable(loc = com[1:50, c("comm_id","lon","lat")])
+# First 5 rows and columns
+distCom$durattion[1:5,1:5]
+```
+<table border=1>
+<tr> <th>  </th> <th> FR3162292119 </th> <th> FR3162212035 </th> <th> BE241011 </th> <th> BE241024 </th> <th> FR3162270520 </th>  </tr>
+  <tr> <td align="right"> FR3162292119 </td> <td align="right"> 0.0 </td> <td align="right"> 5.4 </td> <td align="right"> 95.1 </td> <td align="right"> 91.6 </td> <td align="right"> 7.5 </td> </tr>
+  <tr> <td align="right"> FR3162212035 </td> <td align="right"> 4.9 </td> <td align="right"> 0.0 </td> <td align="right"> 98.3 </td> <td align="right"> 94.7 </td> <td align="right"> 7.1 </td> </tr>
+  <tr> <td align="right"> BE241011 </td> <td align="right"> 94.2 </td> <td align="right"> 97.3 </td> <td align="right"> 0.0 </td> <td align="right"> 10.4 </td> <td align="right"> 93.5 </td> </tr>
+  <tr> <td align="right"> BE241024 </td> <td align="right"> 90.8 </td> <td align="right"> 93.8 </td> <td align="right"> 10.4 </td> <td align="right"> 0.0 </td> <td align="right"> 90.0 </td> </tr>
+  <tr> <td align="right"> FR3162270520 </td> <td align="right"> 7.0 </td> <td align="right"> 6.9 </td> <td align="right"> 93.5 </td> <td align="right"> 90.0 </td> <td align="right"> 0.0 </td> </tr>
+</table>
+
+
+### `osrmRoute`
+
+```r
+library(osrm)
+# Load data
+data("com")
+
+# Travel path between SpatialPointsDataFrame
+route <- osrmRoute(src = src[1,], 
+                   dst = dst[1,],
+                   sp = TRUE)
+plot(route, lty = 2, lwd = 2)
+plot(src[1,], pch = 20, col = "green", cex = 3, add = TRUE)             
+plot(dst[1,], pch = 20, col = "red", cex = 3, add = TRUE) 
+```
+![](http://rgeomatic.hypotheses.org/files/2016/04/viageom.png)
+
+
+### `osrmTrip`
+
+```r
+library(osrm)
+# Load data
+data("com")
+
+# Get a trip with a SpatialPointsDataFrame
+trips <- osrmTripGeom(loc = src)
+
+# Map
+if(require("cartography")){
+  osm <- getTiles(spdf = trips[[1]]$trip, crop = TRUE)
+  tilesLayer(osm)
+  plot(src, pch = 20, col = "red", cex = 2, add = TRUE)
+  plot(trips[[1]]$trip, add = TRUE, lwd=2)
+}
+
+```
+
+![](http://rgeomatic.hypotheses.org/files/2016/04/trips.png)
+
+### `osrmIsochrone`
+
+```r
+library(osrm)
+# Load data
+data("com")
+
+# Get isochones with a SpatialPointsDataFrame, custom breaks
+iso <- osrmIsochrone(loc = src[6,], breaks = seq(from = 0,to = 30, by = 5))
+
+# Map
+if(require("cartography")){
+  osm <- getTiles(spdf = iso, crop = TRUE)
+  tilesLayer(osm)
+  breaks <- sort(c(unique(iso$min), max(iso$max)))
+  pal <- paste(carto.pal("taupe.pal", length(breaks)-1), "95", sep="")
+  
+  cartography::choroLayer(spdf = iso, df = iso@data,
+                          var = "center", breaks = breaks,
+                          border = "grey50", col = pal,
+                          legend.pos = "topleft",legend.frame = TRUE, 
+                          legend.title.txt = "Isochrones\n(min)", 
+                          add = TRUE)
+  plot(src[6,], add=T)
+}
+```
+![](http://rgeomatic.hypotheses.org/files/2016/04/iso.png)
+
 
 ## Installation
 
-### CRAN version
-```{r}
-install.packages("osrm")
-```
-
-### GitHub version :
+* Development version on GitHub
 ```{r}
 require(devtools)
 devtools::install_github("rCarto/osrm")
 ```
 
+* Stable version on [CRAN](https://CRAN.R-project.org/package=osrm/)
+```{r}
+install.packages("osrm")
+```
 
